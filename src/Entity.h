8ee0244e85114a639b4d9ef7bfa5b5cdff492def
Marcelino Coll Rovira <coll.marce@gmail.com>
2017-01-19 20:30:06 +0100

    Fixed Entity error with redefined id

--- src/Entity.h
+++ src/Entity.h
@@ -1,12 +1,14 @@
 #ifndef _ENTITY_
 #define _ENTITY_
 
+#include <array>
 
-enum class EntityID : uint32_t {};
+enum class EntityID : int {};
 
 class EntityManager;
 class Entity final {
 	friend class EntityManager;
+	friend class std::array<Entity, 5000>;
 
 private:
 	Entity();
@@ -15,10 +17,10 @@ public:
 	~Entity();
 
 	// Get the entity ID
-	EntityID id() const { return id };
+	EntityID id() const { return eid; };
 
 private:
-	EntityID id;
+	EntityID eid;
 };
 
 
